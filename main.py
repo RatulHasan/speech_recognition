2b401a8a594db7bce33807427421ba4dac5bdffb
Ratul Hasan <ratuljh@gmail.com>
2018-04-24 16:54:33 +0600

    Initial commit

--- main.py
+++ main.py
@@ -39,6 +39,14 @@ def init_speech():
         print("Say something")
         audio = r.listen(source)
 
+    # write audio to a RAW file
+    with open("microphone-results.raw", "wb") as f:
+        f.write(audio.get_raw_data())
+
+    # write audio to a WAV file
+    with open("microphone-results.wav", "wb") as f:
+        f.write(audio.get_wav_data())
+
     play_audio("./audio/Beep5.wav")
 
     command = ""
